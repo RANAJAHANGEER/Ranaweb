57474399c01b9c345cfd4f6517a4de2d07d3c04f
toshiba <toshiba@DESKTOP-1FFR45Q>
2022-06-29 08:52:18 +0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ranaonline_store.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
