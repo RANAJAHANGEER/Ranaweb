50f03d90f5fd6fe6daf411c0c8621f157d6a8610
RANA JAHANGEER <19101001-108@uskt.edu.pk>
2022-07-02 14:17:19 +0500

    Account HTML page create

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,12 +1,51 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using task3.Models;
 
-namespace Ranaonline_store.Controllers
+namespace task3.Controllers
 {
     public class HomeController : Controller
     {
+        public class HomeController : Controller
+        {
+            public IActionResult Index()
+            {
+                return View();
+            }
+        }
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
+            ViewBag.Message = "Message is Recieved";
             return View();
+        }
+        public IActionResult task(string fname, string lanme, string email, char pno)
+        {
+
+            return View(Index);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
